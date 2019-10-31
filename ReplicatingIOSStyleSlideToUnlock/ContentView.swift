4504374058a89895d70b2de61fa2dde080e7211b
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-31 17:01:08 +0200

    init

--- ReplicatingIOSStyleSlideToUnlock/ContentView.swift
+++ ReplicatingIOSStyleSlideToUnlock/ContentView.swift
@@ -9,8 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var chromaShift = false
+    @State private var slide = false
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Text("Slide to unlock")
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+                .hueRotation(.degrees(chromaShift ? 0 : 720))
+                .animation(Animation.linear(duration: 4).repeatForever(autoreverses: false))
+                .onAppear() {
+                    self.chromaShift.toggle()
+            }
+            Rectangle()
+                .foregroundColor(.white)
+                .frame(maxWidth: 200, maxHeight: 40)
+                .rotationEffect(.degrees(0), anchor: .trailing)
+                .opacity(0.5)
+                .scaleEffect(x: slide ? 0 : 1, y: 1, anchor: .trailing)
+                .animation(Animation.easeInOut(duration: 4).repeatForever(autoreverses: false).speed(2))
+                .onAppear() {
+                    self.slide.toggle()
+            }
+        }
     }
 }
 
